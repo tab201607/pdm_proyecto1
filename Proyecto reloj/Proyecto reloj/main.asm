1d84a6b8c8c4780491a8050cf272e0df1a6d3178
tab201607 <tab201607@uvg.edu.gt>
2024-03-14 02:56:34 -0600

    Finalizado

--- Proyecto reloj/Proyecto reloj/main.asm
+++ Proyecto reloj/Proyecto reloj/main.asm
@@ -35,7 +35,9 @@
 
 .equ timr1reset = 235 ; outer loop time length
 
-.equ timr2reset = 25 ; muxeo debounce time length
+.equ timr2reset = 25 ; muxeo time length
+
+.equ debouncetime = 0xFF ; tiempo del debounce (multiplicar por timr2)
 
 .cseg
 .org 0x00
@@ -161,6 +163,7 @@ SEI ; habilitamos interrupts
  aftereditcheck:
  SBRS state, 7 ; revisamos si el boton cambio de estado se activo
  RJMP afteradvancestate
+ CBR state, 0b1000_0000 ; apagamos el flag del boton
 
  advancestate: ; utilizamos este bloque para avanzar al siguiente estado cuando se marca el boton cambio de estado
  MOV R17, state
@@ -169,11 +172,11 @@ INC R17
 ANDI state, 0b1111_1100
 ANDI R17, 0b0000_0011
 OR state, R17
-CBR state, 0b1000_0000 ; apagamos el flag del boton
- 
+
  afteradvancestate:
  SBRS state, 6 ; revisamos si el boton de decremento de estado se activo
  RJMP displayLoop2
+ CBR state, 0b0100_0000 ; apagamos el flag del boton
 
  decreasestate:
  MOV R17, state
@@ -182,7 +185,7 @@ DEC R17
 ANDI state, 0b1111_1100
 ANDI R17, 0b0000_0011
 OR state, R17
-CBR state, 0b0100_0000 ; apagamos el flag del boton
+
  
 
 
@@ -250,13 +253,16 @@ RJMP editTime
  RJMP displayLoop
 
  Displaydatemode:
+
+ SBRC state, 2 ; si el registro de editar esta encendido saltamos al bloque respectivo
+RJMP editDate
+
  MOV R17, month
  MOV R18, day
  CALL sevensegmentmux
  RJMP displayLoop
 
  Displayalarmmode:
- SBI PORTC, 2
 
  SBRC state, 2 ; si el registro de editar esta encendido saltamos al bloque respectivo
 RJMP editAlarm
@@ -474,11 +480,11 @@ editTimeEnd:
 editTimeDisplay:
 MOV R17, timemin
  MOV R18, timehr
-  CALL sevensegmentmux ; desplegamos el tiempo en horas y minutos en el debounce
+  CALL sevensegmentmux ; desplegamos el tiempo
   RET
 
   ; //////////////////////////////////////
-; Subrutina de editar tiempo
+; Subrutina de editar alarma (igual que cambiar tiempo
 ; //////////////////////////////////////
 
 
@@ -616,15 +622,158 @@ RJMP editalarmmins
 
 editalarmEnd:
   CBR state, 0b0010_0100 ; apagamos el flag de edit y el del boton
+  SBR state, 0b0000_1000 ; encendemos el bit de alarma
 
   RJMP displayLoop ; regresamos el modo display
 
 editalarmDisplay:
 MOV R17, alarmmin
  MOV R18, alarmhr
-  CALL sevensegmentmux ; desplegamos el tiempo en horas y minutos en el debounce
+  CALL sevensegmentmux ; desplegamos la alarma
   RET
 
+  ; //////////////////////////////////////
+; Subrutina de editar fecha
+; //////////////////////////////////////
+
+  editDate: ; cambiar meses
+  CBR state, 0b0010_0000 
+
+  CALL editDateDisplay
+
+  SBRC state, 5 ; si el boton 3 se apacha vamos al estado de cambiar dias
+  RJMP editdayspre
+
+  SBRC state, 6 ; si el boton 1 se apacha vamos al estado de decrementar
+  RJMP decreasemonths
+
+  SBRS state, 7 ; si el boton 2 se apacha incrementamos 
+  RJMP editDate
+
+  CBR state, 0b1000_0000 ; apagamos bandera boton incrementar
+
+  incrementarmeses:
+  INC month
+LDI R16, 10
+MOV R17, month
+ANDI R17, 0x0F 
+CPSE R17, R16 ; revisamos que no haya llegado a 10
+RJMP checkoverflowyear
+
+ANDI month, 0xF0 ; colocamos a contador de dias en 0
+LDI R16, 0x10
+ADD month, R16 ; incrementamos el contador de decenas de meses
+
+checkoverflowyear:
+LDI R16, 0x13
+MOV R17, month
+CPSE R17, R16 ; revisamos si es igual a 13
+RJMP editDate
+LDI month, 0x01 ; colocamos 1
+RJMP editDate
+
+
+decreasemonths:
+CBR state, 0b0100_0000
+
+DEC month
+LDI R16, 0x0F
+MOV R17, month
+ANDI R17, 0x0F 
+CPSE R17, R16 ; revisamos que unidades no haya bajado menor a 0 
+RJMP checkunderflowyear
+
+ANDI month, 0xF9 ; dejamos a unidades meses en 9  
+
+checkunderflowyear:
+LDI R16, 0x00
+CPSE R16, month ; revisamos si estamos en 0 meses
+RJMP editDate
+LDI month, 0x12 ; cargamos 12
+RJMP editDate
+
+
+
+editdayspre:
+LDI day, 0x01 ; reiniciamos el contador de dias
+editDays:
+CBR state, 0b0010_0000
+
+CALL editDateDisplay
+
+  SBRC state, 5 ; si el boton 3 se apacha vamos al estado de cambiar dias
+  RJMP editdateend
+
+  SBRC state, 6 ; si el boton 1 se apacha vamos al estado de decrementar
+  RJMP decreasedays
+
+  SBRS state, 7 ; si el boton 2 se apacha incrementamos 
+  RJMP editDays
+
+  increasedays:
+  CBR state, 0b1000_0000 ; apagamos bandera boton incrementar
+
+  INC day
+LDI R16, 10
+MOV R17, day
+ANDI R17, 0x0F 
+CPSE R17, R16 ; revisamos que no haya llegado a 10
+RJMP checkoverflowday
+
+ANDI day, 0xF0 ; colocamos a contador de dias en 0
+LDI R16, 0x10
+ADD day, R16 ; incrementamos el contador de decenas de dias
+
+checkoverflowday:
+LDI ZL, LOW(monthlengths << 1) ; Seleccionamos el ZL para encontrar al bit bajo en el flash
+LDI ZH, HIGH(monthlengths << 1) ; Seleccionamos el ZH para ecnontar al bit alto en el flash
+MOV R17, month
+DEC R17
+ADD ZL, R17
+LPM R16, Z ; Cargamos el valor del tabla a R16
+INC R16 ; le agregamos uno mas porque ahora el valor del dia es uno mas que el ultimo dia del mes
+
+CPSE R16, day ; revisamos si ha llegado al maximo del dado mes
+RJMP editDays
+LDI day, 0x01 ; reiniciamos el contador de dias
+RJMP editDays
+
+
+decreasedays:
+CBR state, 0b0100_0000
+
+DEC day
+LDI R16, 0x0F
+MOV R17, day
+ANDI R17, 0x0F 
+CPSE R17, R16 ; revisamos que unidades no haya bajado menor a 0 
+RJMP checkunderflowday
+
+ANDI day, 0xF9 ; dejamos a unidades meses en 9  
+
+checkunderflowday: 
+LDI R16, 0x00
+CPSE R16, day ; revisamos si estamos en 0 meses
+RJMP editDays
+LDI ZL, LOW(monthlengths << 1) ; Seleccionamos el ZL para encontrar al bit bajo en el flash
+LDI ZH, HIGH(monthlengths << 1) ; Seleccionamos el ZH para ecnontar al bit alto en el flash
+MOV R17, month
+DEC R17
+ADD ZL, R17
+LPM day, Z ; Cargamos el valor del tabla a dias
+RJMP editDays
+
+
+
+editdateEnd:
+  CBR state, 0b0010_0100 ; apagamos el flag de edit y el del boton
+  RJMP displayLoop ; regresamos el modo display
+
+  editdateDisplay:
+MOV R18, day 
+ MOV R17, month
+  CALL sevensegmentmux ; desplegamos la fecha
+  RET
 
 ; ///////////////////////////////////
 ; Subrutina cambio de tiempo
@@ -735,7 +884,6 @@ ADD ZL, R17
 LPM R16, Z ; Cargamos el valor del tabla a R16
 INC R16 ; le agregamos uno mas porque ahora el valor del dia es uno mas que el ultimo dia del mes
 
-
 CPSE R16, day ; revisamos si ha llegado al maximo del dado mes
 RJMP timecheckend
 
@@ -771,16 +919,18 @@ RET
 ISR_PCINT1: ; Para el cambio de pines
 
 ; debounce
-SBRC debouncetimer, 7 ; revisamos si el debounce es activo, en caso que si no realizamos todo lo demas
+SBRC state, 4 ; revisamos si el debounce es activo, en caso que si no realizamos todo lo demas
 RETI
 
 SBIS PORTC, PC3 ; vemos si la alarma esta encendida
 RJMP afterturningoffalarm
 CBI PORTC, PC3 ; apagamos la alarma cuando cualquier boton se activa
 CBR state, 0b0000_1000 ; tambien apagamos el flag de que la alarma esta encendida
+RETI
 
 afterturningoffalarm:
-LDI debouncetimer, 0XFF ; le colocamos 25.5ms al debouncetimer
+LDI debouncetimer, debouncetime 
+SBR state, 0b0001_0000 ; marcamos que el debounce esta activo
 
 SBIC PINC, PC0 
 RJMP button2check
@@ -805,8 +955,13 @@ ISR_TIMR2:
 LDI R16, timr2reset ; Cargamos 0.1ms al timer2
 STS TCNT2, R16
 
-SBRC debouncetimer, 7 ; revisamos si el debounce esta activo
-DEC debouncetimer ; decrementamos el debounce timer cada 10ms
+SBRS state, 4 ; revisamos si el debounce esta activo
+RJMP afterdebouncecheck
+DEC debouncetimer 
+BRNE afterdebouncecheck
+CBR state, 0b0001_0000 ; apagamos  el debounce
+
+afterdebouncecheck:
 
 ; Control de muxeo de los 7 segment displays
 LSL muxshow
@@ -814,6 +969,7 @@ SBRC muxshow, 4
 LDI muxshow, 0x01
 
 endtimr2:
+
 SBI TIFR2, 0 ; Colocamos un 0 TV0 para reiniciar el timer
 RETI
 
@@ -824,31 +980,69 @@ RETI
 
 ISR_TIMR0: ; Para el cambio de timer0
 
-SBRC state, 7
-RJMP endtimr0
-
 LDI R16, timr1reset ; Cargamos 10ms al timer0
 OUT TCNT0, R16
 
+SBRC state, 2 ; revisamos si estamos en modo de editar
+RJMP edit750
+
+MOV R16, state 
+ANDI R16, 0x03 
+CPI R16, 0x02 ; state de fecha
+BREQ outerloopdecrease ; apagamos las luces si estamos en modo fecha
+
+CPI R16, 0x03 ; state de alarma
+BREQ outerloopdecrease ; queremos mantener la luz prendida en modo alarma
+
+RJMP nonedit500
+
+edit750: ; encender la luz en 750 ms si estamos en modo de editar
+LDI R16, 75
+CPSE outerloop, R16
+RJMP edit500
+SBI PORTC, PC5
+RJMP edit500
+
+nonedit500: ; encender la luz en 500ms si no estamos en modo de editar
 LDI R16, 50 
 CPSE outerloop, R16 ; revisamos si han pasado 500ms
 RJMP outerloopdecrease
-CBI PORTC, PC5 ; encendemos los luces de por medio
+SBI PORTC, PC5 ; encendemos los luces de por medio
+RJMP outerloopdecrease
+
+edit500: ; apagar la luz en 500ms si estamos en modo editar
+LDI R16, 50
+CPSE outerloop, R16
+RJMP edit250
+CBI PORTC, PC5
+
+edit250: ; modo editar - encender la luz en 250ms
+LDI R16, 25
+CPSE outerloop, R16
+RJMP outerloopdecrease
+SBI PORTC, PC5
 
 outerloopdecrease:
 DEC outerloop
 BRNE endtimr0
 
-MOV outerloop, state ; por ahora podemos utilizar outerloop para otra funcion
-ANDI outerloop, 0x03 ; lo utilizamos ya que R17 o R18 podrian estar en uso
-LDI R16, 0x02 ; state de alarma
+MOV R16, state
+ANDI R16, 0x03
+CPI R16, 0x03 ; state de alarma
+BREQ alarmstateactive ; queremos mantener la luz prendida en modo alarma
 
-CPSE outerloop, R16 ; no encendemos las luces si estamos en modo de alarma
-SBI PORTC, PC5 ; ahora que otros 500ms han pasado los apagamos
+CBI PORTC, PC5 ; modo editar o normal - apagar la luz en 0ms
 LDI outerloop, 100 ; le cargamos 100 al segundo loop 
 
-SBRS state, 0b0000_0100 ; si el flag de editar esta encendido no activamos la subrutina cambio del tiempo
+SBRC state, 0b0000_0100 ; si el flag de editar esta encendido no activamos la subrutina cambio del tiempo
+RJMP endtimr0
 CALL timecheck ; rutina cambio de tiempo
 
 endtimr0:
+RETI
+
+alarmstateactive:
+SBI PORTC, PC5 ;siempre encendemos PC5 en caso que si
+
+CALL timecheck
 RETI
